3faffacb48643a6e48e3cb63258dc04e42daeca3
fouric <fouric@openmailbox.org>
2016-10-29 13:32:59 -0700

    it works now! sort of

--- src/canebreak.lisp
+++ src/canebreak.lisp
@@ -14,49 +14,55 @@
   (asdf:system-relative-pathname 'canebreak path))
 
 (defun canebreak (in-filename out-filename)
+  (setf *indentation-level* 0)
   (with-open-file (out out-filename
 		       :direction :output
 		       :if-exists :supersede)
     (setf *out-file* out)
-    (mapcar #'process (read-file in-filename))
+    (mapcar (lambda (f) (process f t)) (read-file in-filename))
     (setf *out-file* nil)))
 
 (defun strcat (&rest strings)
   (apply #'concatenate (cons 'string strings)))
 
-(defun process (form)
+(defun process (form &optional (emit nil))
   (format t "~a~%" form)
-  (emit
-   (etypecase form
-     (string
-      form)
-     (number
-      (format t "~a" form))
-     (symbol
-      (if (member form *registers*)
-	  (string-downcase form)))
-     (list (let ((command-name (first form)))
-	     (cond 
-	       ((eql command-name 'file)
-		(strcat ".file \"" (nth 1 form) "\""))
-	       ((eql command-name 'intel-syntax)
-		(strcat ".intel_syntax " (string-downcase (nth 1 form))))
-	       ((eql command-name 'text)
-		".text")
-	       ((or (eql command-name 'global) (eql command-name 'globl))
-		(strcat ".global " (string-downcase (nth 1 form))))
-	       ((eql command-name 'type)
-		(strcat ".type " (string-downcase (nth 1 form)) ", " (string-downcase (nth 2 form))))
-	       ((eql command-name 'section)
-		(let ((name (nth 1 form)))
-		  (concatenate 'string (string-downcase name) ":")
-		  (incf *indentation-level*)
-		  (mapcar #'process (rest (rest form)))
-		  (decf *indentation-level*)))
-	       ((member (first form) *instructions*)
-		(apply #'strcat (append (list (string-downcase (first form))) (join-commas (mapcar #'process (rest form))))))))))))
+  (let ((retval
+	 (etypecase form
+	   (string
+	    form)
+	   (number
+	    (format nil "~a" form))
+	   (symbol
+	    (if (member form *registers*)
+		(string-downcase form)))
+	   (list
+	    (let ((command-name (first form)))
+	      (cond 
+		((eql command-name 'file)
+		 (strcat ".file \"" (nth 1 form) "\""))
+		((eql command-name 'intel-syntax)
+		 (strcat ".intel_syntax " (string-downcase (nth 1 form))))
+		((eql command-name 'text)
+		 ".text")
+		((or (eql command-name 'global) (eql command-name 'globl))
+		 (strcat ".global " (string-downcase (nth 1 form))))
+		((eql command-name 'type)
+		 (strcat ".type " (string-downcase (nth 1 form)) ", " (string-downcase (nth 2 form))))
+		((eql command-name 'section)
+		 (let ((name (nth 1 form)))
+		   (emit (concatenate 'string (string-downcase name) ":"))
+		   (incf *indentation-level*)
+		   (mapcar (lambda (f) (process f t)) (rest (rest form)))
+		   (decf *indentation-level*)))
+		((member (first form) *instructions*)
+		 (apply #'strcat (append (list (string-downcase (first form)) " " (apply #'join-commas (mapcar #'process (rest form)))))))))))))
+    (when emit
+      (emit retval))
+    retval))
 
-(defparameter *registers* '(rax rbx rcx rdx rsi rdi rbp rsp r8 r9 r10 r11 r12 r13 r14 r15))
+(defparameter *registers* '(rax rbx rcx rdx rsi rdi rbp rsp r8 r9 r10 r11 r12 r13 r14 r15
+			    eax ebx ecx edx esi edi edp esp))
 (defparameter *instructions* '(push mov pop ret))
 
 (defun emit (object)
--- src/out.s
+++ src/out.s
@@ -1,21 +1,14 @@
-                    .file "test.c"
-                    .intel_syntax noprefix
-                    .text
-                    .global main
-                    .type main, @function
-                        rbp
-                        pushrbp
-                        rbp
-                        rsp
-                        movrbprsp
-                        DWORD PTR [rbp-4]
-                        NIL
-                        movDWORD PTR [rbp-4]
-                        NIL
-                        DWORD PTR [rbp-4]
-                        movDWORD PTR [rbp-4]
-                        rbp
-                        poprbp
-                        ret
-                    5
-                    NIL
+.file "test.c"
+.intel_syntax noprefix
+.text
+.global main
+.type main, @function
+main:
+    push rbp
+    mov rbp, rsp
+    mov DWORD PTR [rbp-4], 42
+    mov eax, DWORD PTR [rbp-4]
+    pop rbp
+    ret 
+0
+NIL
